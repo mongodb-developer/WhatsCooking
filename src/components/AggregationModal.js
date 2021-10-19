d224eaa90fdae090912865c0fb0b8d24413ec40c
Karen Huaulme <karen.huaulme@gmail.com>
2021-10-18 19:46:24 -0500

    react syntax highlighter on Aggregation Modal

--- src/components/AggregationModal.js
+++ src/components/AggregationModal.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const AggregationModal = ({ setShowAggregation, stages }) => {
   var jsonSearch = JSON.stringify(JSON.parse(stages.searchStage), null, 2);
@@ -6,26 +8,29 @@ const AggregationModal = ({ setShowAggregation, stages }) => {
   var jsonProject = JSON.stringify(JSON.parse(stages.projectStage), null, 2);
 
   return (
-    <div className="fixed inset-0 z-20 flex justify-center bg-smoke-dark">
-      <div className="relative px-20 py-10 mx-auto mt-24 overflow-x-scroll overflow-y-auto text-2xl text-white whitespace-pre-wrap bg-black rounded h-3/4 ">
-        <div className="text-4xl text-center text-yellow-300">
+    <div className="fixed inset-0 z-20 flex justify-center bg-smoke-dark font-body">
+      <div className="relative px-20 py-10 mx-auto mt-24 overflow-x-scroll overflow-y-auto text-2xl text-white whitespace-pre-wrap bg-black h-3/4 ">
+        <div className="text-4xl text-center text-deep-cerulean-200 font-bold">
           Aggregation Pipeline with $search
         </div>
         <br></br>
-        <div className="text-white ">[</div>
-        <div className="divide-y-4 divide-yellow-300">
-          <pre className="mb-10">{jsonSearch},</pre>
-          <pre className="py-10 mb-10 ml-10 ">{jsonLimit},</pre>
-          <pre className="pt-10 ml-10">{jsonProject}</pre>
-        </div>
-        <div className="text-white ">]</div>
-        <br></br>
+
+        <SyntaxHighlighter language="javascript" style={atomDark}>
+          {jsonSearch}
+        </SyntaxHighlighter>
+        <SyntaxHighlighter language="javascript" style={atomDark}>
+          {jsonLimit}
+        </SyntaxHighlighter>
+        <SyntaxHighlighter language="javascript" style={atomDark}>
+          {jsonProject}
+        </SyntaxHighlighter>
+
         <svg
           xmlns="http://www.w3.org/2000/svg"
           onClick={() => {
             setShowAggregation(false);
           }}
-          className="sticky bottom-0 float-right w-16 pr-0 text-red-500"
+          className="sticky bottom-0 float-right w-16 pr-0 text-fuchsia-500"
           viewBox="0 0 20 20"
           fill="currentColor"
         >
@@ -41,3 +46,20 @@ const AggregationModal = ({ setShowAggregation, stages }) => {
 };
 
 export default AggregationModal;
+
+/**********
+ *  <div className="text-white ">[</div>
+        <div className="divide-y-4 divide-yellow-300 ">
+          <pre className="mb-10 font-body  text-2xl">{jsonSearch},</pre>
+          <pre className="py-10 mb-10 ml-10 font-body text-2xl">
+            <SyntaxHighlighter language="javascript" style={coldarkDark}>
+              {jsonLimit}
+            </SyntaxHighlighter>
+            ,
+          </pre>
+          <pre className="pt-10 ml-10 font-body text-2xl">{jsonProject}</pre>
+        </div>
+        <div className="text-white ">]</div>
+ * 
+ * 
+ */
