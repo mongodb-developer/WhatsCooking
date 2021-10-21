d5c87d885b36a7295af9bfdfa67a5cb5f31bd1fe
Karen Huaulme <karen.huaulme@gmail.com>
2021-10-21 11:31:51 -0500

    synonyms page formatting - add GET and POST requests for synonyms

--- src/pages/SynonymsPage.js
+++ src/pages/SynonymsPage.js
@@ -1,60 +1,101 @@
-import React, { useState } from "react";
-import { useForm, Controller, useFieldArray } from "react-hook-form";
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 const SynonymsPage = () => {
+  const [loadedSynonyms, setLoadedSynonyms] = useState([]);
   const [equivalentMapping, setEquivalentMapping] = useState(true);
   const { register, handleSubmit, watch, control, errors, reset, setValue } =
-    useForm();
+    useForm({
+      word: "",
+      synonyms: [],
+      mapping: "equivalent",
+    });
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const getSynonyms = async () => {
+    let storedSynonyms = await (
+      await fetch(
+        "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whatscooking-agtge/service/synonyms/incoming_webhook/getFoodSynonyms"
+      )
+    ).json();
+    console.log("SYNONYMS", storedSynonyms);
   };
+  useEffect(() => {
+    getSynonyms();
+  }, []); // add all external values your effect function depends on - none in this case
+
+  const addSynonym = (data) => {
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    fetch(
+      "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/whatscooking-agtge/service/synonyms/incoming_webhook/addSynonyms",
+      requestOptions
+    ).then(() => {
+      console.log("SUBMITTED SYNONYM!!");
+      //history.replace("/");
+    }); // push goes back to previous page - so replace // fetch returns a promise
+  };
+
+  // const onSubmit = (data) => {
+  //   console.log(data);
+  // };
   return (
     <>
       <div className="text-4xl text-center my-8">Synonyms</div>
 
-      {/* <div className="flex justify-center items-center">
-        <div className="font-body text-2xl">Equivalent</div>
-        <label htmlFor="toggle-switch">
-          <input
-            type="checkbox"
-            id="toggle-switch"
-            className="cursor-pointer mx-10 h-16 w-32 rounded-full appearance-none bg-gray-800 border-4 border-black checked:bg-gray-800 transition duration-200 relative"
-            checked={equivalentMapping}
-            onChange={() => setEquivalentMapping(!equivalentMapping)}
-          />
-        </label>
-        <div className="font-body text-2xl">Explicit</div>
-      </div> */}
+      <form
+        className="flex my-24 w-3/4 justify-between ml-20 border border-gray-300 shadow p-3 rounded "
+        onSubmit={handleSubmit(addSynonym)}
+      >
+        <div className="flex flex-col w-1/3">
+          <div className="flex items-center">
+            <label
+              htmlFor="word"
+              className="inline-block text-right mr-6 w-20 font-bold font-body text-2xl"
+            >
+              Word:
+            </label>
+            <input
+              type="text"
+              placeholder="word"
+              name="word"
+              autoComplete="off"
+              {...register("word", { required: true })}
+              className="border-b-2 border-gray-400 focus:border-green-800 w-full flex-1 py-2 placeholder-gray-300 outline-none font-body text-2xl"
+            />
+          </div>
 
-      <form className="flex my-24 ml-10" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          placeholder="word"
-          name="word"
-          {...register("word", { required: true })}
-          className="text-2xl"
-        />
-
-        {/* <Controller */}
-        <div className="flex justify-center items-center">
-          <div className="font-body text-2xl">Equivalent</div>
           <label htmlFor="toggle-switch">
             <input
               type="checkbox"
               name="mapping"
               id="toggle-switch"
               defaultChecked={true}
-              className="cursor-pointer mx-10 h-16 w-32 rounded-full appearance-none bg-gray-800 border-4 border-black checked:bg-gray-800 transition duration-200 relative"
+              className="cursor-pointer mx-24 h-10 my-10 w-24 rounded-full appearance-none bg-gray-800 border-4 border-black checked:bg-gray-800 transition duration-200 relative"
               onChange={() => setEquivalentMapping(!equivalentMapping)}
               {...register("mapping")}
             />
           </label>
-          <div className="font-body text-2xl">Explicit</div>
+
+          <div className="font-body text-2xl mx-4">Equivalent --- Explicit</div>
+        </div>
+        <div id="synonyms">
+          <textarea
+            rows="6"
+            cols="20"
+            className="border border-gray-400 p-5 outline-none focus:border-tolopea-400 rows=10"
+            placeholder={`synonyms separated by a comma...`}
+            name={`synonyms`}
+            {...register(`synonyms`)}
+          />
         </div>
 
         <input
-          className="px-8 py-4 bg-indigo-800 text-white rounded"
+          className="px-8 py-4 bg-indigo-800 text-white rounded h-16 my-auto"
           type="submit"
         />
       </form>
@@ -65,3 +106,4 @@ const SynonymsPage = () => {
 };
 
 export default SynonymsPage;
+/**<div className="flex justify-center items-center mt-10"> */
