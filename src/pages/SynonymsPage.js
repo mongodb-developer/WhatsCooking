8543e86d737006970d3e0f1e558cb740a382bf53
Karen Huaulme <karen.huaulme@gmail.com>
2021-10-21 20:27:39 -0500

    fixed state in synonyms

--- src/pages/SynonymsPage.js
+++ src/pages/SynonymsPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import SynonymForm from "../components/SynonymForm";
-import FoodIdeasIcon from "../images/foodIdeas.png";
 
 const SynonymsPage = () => {
   const [loadedSynonyms, setLoadedSynonyms] = useState([]);
@@ -22,17 +21,17 @@ const SynonymsPage = () => {
   return (
     <>
       <div className="text-4xl text-center my-8">Synonyms</div>
-      <div className="flex flex-col w-full h-full bg-foodIdeas bg-contain bg-no-repeat bg-right-bottom bg-scroll">
-        <div className="grid grid-cols-3 gap-6 p-2 mt-10 md:grid-cols-4 md:gap-6">
-          {loadedSynonyms?.map((doc) => {
-            <div key={doc._id} className="w-1/8 text-lg font-bold ">
-              {doc.mappingType}
-            </div>;
-          })}
-        </div>
-
-        <SynonymForm />
+      {/* <div className="flex flex-col w-full h-full bg-foodIdeas bg-contain bg-no-repeat bg-right-bottom bg-scroll"> */}
+      <div className="grid grid-cols-3 gap-6 p-2 mt-10 md:grid-cols-4 md:gap-6">
+        {loadedSynonyms.map((syn) => (
+          <div className="" key={syn._id} clickable synId={syn._id}>
+            {syn.mappingType}
+          </div>
+        ))}
       </div>
+
+      <SynonymForm />
+      {/* </div> */}
     </>
   );
 };
