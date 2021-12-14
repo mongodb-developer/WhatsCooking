91a61a050b90ba61f390bae50678982b1ffe9b22
Karen Huaulme <karen.huaulme@gmail.com>
2021-12-14 10:30:50 -0600

    re-work the synonyms page with document examples

--- src/pages/SynonymsPage.js
+++ src/pages/SynonymsPage.js
@@ -6,6 +6,8 @@ import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 import Icon from "../images/whatscooking.png";
 import IdeasIcon from "../images/foodIdeas.png";
+import SYNCOL from "../images/SynCol.png";
+import DocHero from "../images/DocModelHero.JPG";
 
 const { REACT_APP_GETSYNONYMS_SECRET } = process.env;
 
@@ -30,6 +32,17 @@ const SynonymsPage = () => {
     console.log("GETTING SYNONYMS");
   }, [deleteMessage, submissionMessage]); // add all external values your effect function depends on - none in this case
 
+  const equivalentDocument = {
+    mappingType: "equivalent",
+    synonyms: ["pie", "cobbler", "tart"],
+  };
+
+  const explicitDocument = {
+    mappingType: "explicit",
+    input: ["baked goods"],
+    synonyms: ["bread", "cake", "cookies", "rolls"],
+  };
+
   const synonymIndex = {
     synonyms: [
       {
@@ -42,6 +55,8 @@ const SynonymsPage = () => {
     ],
   };
 
+  const equivalentString = JSON.stringify(equivalentDocument, null, 2);
+  const explicitString = JSON.stringify(explicitDocument, null, 2);
   const synIndexString = JSON.stringify(synonymIndex, null, 2);
 
   return (
@@ -62,80 +77,129 @@ const SynonymsPage = () => {
         </div>
       </div>
 
-      <div className="flex space-x-8 mx-16 text-2xl justify-center">
-        <div className="w-1/4 my-auto">
-          <img src={IdeasIcon} alt="thinking" className="my-auto"></img>
-        </div>
-        <div className="my-auto text-center">
-          <div className="text-center text-3xl">
-            You say "pop," and I say "coke?"{" "}
-            <span className="text-6xl">🥤</span>
-          </div>
-          <div className="text-center text-2xl">
-            <br></br>With synonyms in Atlas Search, call it what you will.
+      <div className="flex flex-col">
+        <div className="flex mx-12 text-2xl justify-evenly">
+          <div className="w-1/4 my-auto">
+            <img src={IdeasIcon} alt="thinking" className="my-auto"></img>
           </div>
 
-          <button
-            type="button"
-            className="flex justify-center my-auto mt-12 w-4/5 mx-auto h-16 pl-4 space-x-4 text-3xl text-white rounded bg-gradient-to-r from-mongo-500 to-green-700 font-body hover:shadow-2xl hover:bg-green-700 focus:outline-none"
-            // onClick={() => {
-            //   setShowSynForm(true);
-            //   setSubmissionMessage("");
-            // }}
-          >
-            <span className="my-auto">Our Custom Synonyms</span>
-            <div className="flex items-center justify-center w-16 h-16 text-6xl rounded-full bg-white">
-              🍽️
+          <div className="my-auto w-1/4 text-center">
+            <div className="text-center text-3xl">
+              You say "pop," and I say "coke?"{" "}
+              <span className="text-6xl">🥤</span>
             </div>
-          </button>
-        </div>
+            <div className="text-center text-2xl">
+              <br></br>With synonyms in Atlas Search, call it what you will.
+            </div>
 
-        <div className="w-auto ml-10 rounded text-base my-auto p-4">
-          <SyntaxHighlighter language="javascript" style={okaidia}>
-            {synIndexString}
-          </SyntaxHighlighter>
-        </div>
-      </div>
-      {submissionMessage !== "" && (
-        <div
-          className="flex justify-center px-2 w-4/5 mt-4 py-4 text-2xl mx-auto bg-yellow-300 rounded"
-          onClick={() => setSubmissionMessage("")}
-        >
-          {submissionMessage}
-        </div>
-      )}
+            <button
+              type="button"
+              className="flex justify-center my-auto mt-12 w-4/5 mx-auto h-16 pl-4 space-x-4 text-3xl text-white rounded bg-gradient-to-r from-mongo-500 to-green-700 font-body hover:shadow-2xl hover:bg-green-700 focus:outline-none"
+              // onClick={() => {
+              //   setShowSynForm(true);
+              //   setSubmissionMessage("");
+              // }}
+            >
+              <span className="my-auto">Our Custom Synonyms</span>
+              <div className="flex items-center justify-center w-16 h-16 text-6xl rounded-full bg-white">
+                🍽️
+              </div>
+            </button>
+          </div>
 
-      {showSynForm && (
-        <SynonymForm
-          className="mx-auto justify-center"
-          setShowSynForm={setShowSynForm}
-          setSubmissionMessage={setSubmissionMessage}
-          setDeleteMessage={setDeleteMessage}
-        />
-      )}
-
-      <div className="grid grid-cols-3 gap-6 p-2 mt-10 px-20 md:grid-cols-3 md:gap-6">
-        {loadedSynonyms.map((syndoc, index) => (
-          <SynonymCard
-            key={syndoc._id}
-            syndoc={syndoc}
-            setDeleteMessage={setDeleteMessage}
-            setUpdateMessage={setUpdateMessage}
-            setShowSynForm
-            setSubmissionMessage={setSubmissionMessage}
-            updateMessage={updateMessage}
-            index={index}
+          {showSynForm ? (
+            <SynonymForm
+              className="mx-auto justify-center"
+              setShowSynForm={setShowSynForm}
+              setSubmissionMessage={setSubmissionMessage}
+              setDeleteMessage={setDeleteMessage}
+            />
+          ) : (
+            <div className="w-1/4 my-auto">
+              <img
+                src={SYNCOL}
+                alt="AtlasUI"
+                className="my-auto border-4 border-light-blue-500 border-opacity-25 shadow-2xl h-128"
+              ></img>
+            </div>
+          )}
+        </div>
+        <div className="mx-24 my-4">
+          {" "}
+          <hr
+            style={{
+              color: "darkgreen",
+              backgroundColor: "darkgreen",
+              height: 1,
+              borderColor: "darkgreen",
+            }}
           />
-        ))}
-      </div>
-      {deleteMessage !== "" && (
-        <div
-          className="flex justify-center px-2 w-4/5 mt-4 py-4 text-2xl mx-auto bg-san-juan-400 text-white rounded"
-          onClick={() => setDeleteMessage("")}
-        >
-          {deleteMessage}
         </div>
-      )}
+
+        <div className="flex mx-16 text-2xl justify-around">
+          <div className="w-1/4 ml-10 rounded text-base p-4">
+            <div className="text-2xl font-body text-center font-bold text-indigo-800 ">
+              Equivalent Synonym Document
+            </div>
+
+            <div className="w-auto rounded text-base my-auto p-4">
+              <SyntaxHighlighter language="javascript" style={okaidia}>
+                {equivalentString}
+              </SyntaxHighlighter>
+            </div>
+          </div>
+
+          <div className="w-1/4  rounded text-base p-4">
+            <div className="text-2xl font-body text-center font-bold text-indigo-800 ">
+              Explicit Synonym Document
+            </div>
+
+            <div className="w-auto rounded text-base my-auto p-4">
+              <SyntaxHighlighter language="javascript" style={okaidia}>
+                {explicitString}
+              </SyntaxHighlighter>
+            </div>
+          </div>
+
+          <div className="w-64 my-auto">
+            <img src={DocHero} alt="thinking" className="my-auto"></img>
+          </div>
+
+          <div className="w-1/4  rounded text-base px-4">
+            <div className="text-2xl font-body text-center font-bold text-indigo-800 ">
+              Add to Search Index to Specify <br></br>Synonyms Source Collection
+            </div>
+            <div className="w-auto rounded text-base my-auto px-4">
+              <SyntaxHighlighter language="javascript" style={okaidia}>
+                {synIndexString}
+              </SyntaxHighlighter>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 gap-6 p-2 mt-10 px-20 md:grid-cols-3 md:gap-6">
+          {loadedSynonyms.map((syndoc, index) => (
+            <SynonymCard
+              key={syndoc._id}
+              syndoc={syndoc}
+              setDeleteMessage={setDeleteMessage}
+              setUpdateMessage={setUpdateMessage}
+              setShowSynForm
+              setSubmissionMessage={setSubmissionMessage}
+              updateMessage={updateMessage}
+              index={index}
+            />
+          ))}
+        </div>
+        {deleteMessage !== "" && (
+          <div
+            className="flex justify-center px-2 w-4/5 mt-4 py-4 text-2xl mx-auto bg-san-juan-400 text-white rounded"
+            onClick={() => setDeleteMessage("")}
+          >
+            {deleteMessage}
+          </div>
+        )}
+      </div>
     </>
   );
 };
